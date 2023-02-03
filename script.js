14d921335a0d78d47425fd4a36708d4f60b0b831
Gulekk <gulekk1@gmail.com>
2023-02-03 16:44:27 +0100

    JS comming

---
+++ script.js
@@ -0,0 +1,11 @@
+var openTrigger = $('.menu-trigger');
+var openTriggerTop = openTrigger.find('.menu-trigger-bar.top');
+var openTriggerMiddle = openTrigger.find('.menu-trigger-bar.middle');
+var openTriggerBottom = openTrigger.find('.menu-trigger-bar.bottom');
+
+var closeTrigger = $('.close-trigger');
+var closeTriggerLeft = closeTrigger.find('.close-trigger-bar.left');
+var closeTriggerRight = closeTrigger.find('.close-trigger-bar.right');
+var logo = $('.logo');
+var menuContainer = $('.menu-container');
+var menu = $('.menu');
